7eaf4d849ab2d5e37ac70d8bc9c5a0dc8646253a
Jash <100846378+JASH-PATEL-6566@users.noreply.github.com>
2022-11-14 18:58:07 +0530

    #6 Add dataTabel

--- frontend/pages/user/items/index.js
+++ frontend/pages/user/items/index.js
@@ -1,6 +1,10 @@
 import classes from './items.module.css';
 import Navbar from "../../../Components/subNavbar/navbar";
 import Head from 'next/Head';
+import DataTable from '../../../Components/DataTabel/DataTabel';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Items = () => {
     return (
@@ -10,6 +14,20 @@ const Items = () => {
             </Head>
             <div className={classes.main_container}>
                 <Navbar title="Items" />
+                <div className={classes.dataTabelContainer}>
+                    <div className={classes.input_container}>
+                        <TextField
+                            className={classes.input}
+                            id="standard-basic"
+                            label="Standard"
+                            variant="standard"
+                        />
+                        <IconButton>
+                            <SearchIcon />
+                        </IconButton>
+                    </div>
+                    <DataTable />
+                </div>
             </div>
         </>
     )
